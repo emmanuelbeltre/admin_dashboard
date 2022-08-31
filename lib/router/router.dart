2ae736f3489fb0dbf15f57c6c5b42b4d171ee9a6
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-08-31 17:46:08 -0400

    Fluro Router implemented

---
+++ lib/router/router.dart
@@ -0,0 +1,26 @@
+import 'package:admin_dashboard/router/admin_handlers.dart';
+import 'package:fluro/fluro.dart';
+
+class Flurorouter {
+  static final FluroRouter router = new FluroRouter();
+
+//initial page route
+  static String rootRoute = '/';
+  //Auth router
+  static String loginRoute = '/auth/login';
+  static String registerRoute = '/auth/register';
+
+  //Dashboard Router
+  static String dashboardRoute = '/auth/dashboard';
+
+  //Auth Routes
+  static void configureRoutes() {
+//Auth routes
+
+    router.define(rootRoute, handler: AdminHandlers.login);
+    router.define(loginRoute, handler: AdminHandlers.login);
+
+    //TODO: handler to register page
+    // router.define(registerRoute, handler: handler);
+  }
+}
