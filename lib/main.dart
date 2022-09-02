b94a26d7c87c4050bbcfdf0d5df3cb00b11c9b45
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-09-02 11:02:42 -0400

    AuthLayout design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   Flurorouter.configureRoutes();
--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -6,10 +6,43 @@ class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Auth Layout'),
-        ),
+        body: ListView(
+      children: [
+        //Desktop
+        _DesktopBody()
+        //Mobile
+        //Liks Bar
+      ],
+    ));
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.black,
+      child: Row(
+        children: [
+          // Twitter Background
+          Expanded(
+              child: Container(
+            color: Colors.blueGrey,
+          )),
+
+          //View Container
+          Container(
+            width: 600,
+            height: double.infinity,
+            color: Colors.black,
+          )
+        ],
       ),
     );
   }
