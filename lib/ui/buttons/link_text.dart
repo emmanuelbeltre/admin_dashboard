643611d9278be1f2921f0bddd1c98e4388620409
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-09-03 11:51:38 -0400

    .

--- lib/ui/buttons/link_text.dart
+++ lib/ui/buttons/link_text.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
-class LinkText extends StatelessWidget {
+class LinkText extends StatefulWidget {
   final String text;
+  final Function? onPressed;
+  const LinkText({super.key, required this.text, this.onPressed});
 
-  const LinkText({super.key, required this.text});
+  @override
+  State<LinkText> createState() => _LinkTextState();
+}
+
+class _LinkTextState extends State<LinkText> {
+  bool isHover = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Text(
-        this.text,
-        style: TextStyle(
-            fontSize: 16,
-            color: Colors.grey[700],
-            decoration: TextDecoration.underline),
+    return GestureDetector(
+      onTap: () {
+        if (widget.onPressed != null) widget.onPressed!();
+      },
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: ((event) => setState(() {
+              isHover = true;
+            })),
+        onExit: ((event) => setState(() {
+              isHover = false;
+            })),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Text(
+            this.widget.text,
+            style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+                decoration:
+                    isHover ? TextDecoration.underline : TextDecoration.none),
+          ),
+        ),
       ),
     );
   }
--- lib/ui/layouts/auth/widgets/links_bar.dart
+++ lib/ui/layouts/auth/widgets/links_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:flutter/material.dart';
 
@@ -6,24 +8,26 @@ class LinksBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Function myFunction;
+
     final size = MediaQuery.of(context).size;
     return Container(
         color: Colors.black,
         height: size.height * 0.05,
         child: Wrap(
-          children: const [
-            LinkText(text: 'About'),
-            LinkText(text: 'Help Center'),
-            LinkText(text: 'Terms of Service'),
-            LinkText(text: 'Privacy Policy'),
-            LinkText(text: 'Cookie Policy'),
-            LinkText(text: 'Ads Info'),
-            LinkText(text: 'Blog'),
-            LinkText(text: 'Status'),
-            LinkText(text: 'Carrers'),
-            LinkText(text: 'Brand Resources'),
-            LinkText(text: 'Advertising'),
-            LinkText(text: 'Marketing '),
+          children: [
+            LinkText(text: 'About', onPressed: () => print('About')),
+            const LinkText(text: 'Help Center'),
+            const LinkText(text: 'Terms of Service'),
+            const LinkText(text: 'Privacy Policy'),
+            const LinkText(text: 'Cookie Policy'),
+            const LinkText(text: 'Ads Info'),
+            const LinkText(text: 'Blog'),
+            const LinkText(text: 'Status'),
+            const LinkText(text: 'Carrers'),
+            const LinkText(text: 'Brand Resources'),
+            const LinkText(text: 'Advertising'),
+            const LinkText(text: 'Marketing '),
           ],
         ));
   }
